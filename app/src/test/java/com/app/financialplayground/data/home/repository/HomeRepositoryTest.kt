1e7e5e143ff684a81f6a0cbea92491e7f0c51b14
unknown <aavila.bio@gmail.com>
2020-10-20 18:18:55 -0300

    include home repo test

--- app/src/test/java/com/app/financialplayground/data/home/repository/HomeRepositoryTest.kt
+++ app/src/test/java/com/app/financialplayground/data/home/repository/HomeRepositoryTest.kt
@@ -1,71 +1,56 @@
-package com.app.financialplayground.ui.home
+package com.app.financialplayground.data.home.repository
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
-import com.app.financialplayground.MainCoroutineRule
-import com.app.financialplayground.data.ViewState
+import com.app.financialplayground.data.home.api.HomeApiService
 import com.app.financialplayground.data.home.model.*
-import com.app.financialplayground.data.home.repository.HomeRepository
-import com.app.financialplayground.getOrAwaitValue
-import com.google.common.truth.Truth.assertThat
+import com.google.gson.annotations.SerializedName
+import com.haroldadmin.cnradapter.NetworkResponse
+import io.mockk.MockKAnnotations
 import io.mockk.coEvery
-
 import io.mockk.coVerify
-import io.mockk.mockk
+import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
+import org.junit.Assert.*
+import org.junit.Before
+
 @ExperimentalCoroutinesApi
-class HomeViewModelTest {
+class HomeRepositoryTest {
 
-    private lateinit var homeViewModel: HomeViewModel
-
-    private val homeRepository = mockk<HomeRepository>(relaxed = true)
-
-    @ExperimentalCoroutinesApi
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
-
-    @get:Rule
-    var instantExecutorRule = InstantTaskExecutorRule()
+    private lateinit var repository: HomeRepository
+    @MockK
+    lateinit var service: HomeApiService
 
     @Before
-    fun setupViewModel() {
-        homeViewModel = HomeViewModel(homeRepository)
+    fun setup() {
+        MockKAnnotations.init(this)
+        repository = HomeRepository(service)
     }
+
 
     @Test
-    fun fetchFinancesSuccess_returnsSuccess() = mainCoroutineRule.runBlockingTest {
-        val expectedResult = ViewState.Success(getExpectedResults())
-        coEvery { homeRepository.getFinances() } returns expectedResult
+    fun getFinances_callsService() = runBlockingTest {
+        val response = NetworkResponse.Success(getFinances(), null, 200)
+        coEvery { service.getFinances() } returns response
 
-        homeViewModel.fetchFinances()
-
-        coVerify(exactly = 1) {
-            homeRepository.getFinances()
-        }
-
-        assertEquals(expectedResult, homeViewModel.viewStateLiveData.getOrAwaitValue())
-    }
-
-    @Test
-    fun fetchFinancesError_returnsError() = mainCoroutineRule.runBlockingTest {
-        val expectedResult = ViewState.Error("error")
-        coEvery { homeRepository.getFinances() } returns expectedResult
-
-        homeViewModel.fetchFinances()
+        repository.getFinances()
 
         coVerify(exactly = 1) {
-            homeRepository.getFinances()
+            service.getFinances()
         }
-
-        assertEquals(expectedResult, homeViewModel.viewStateLiveData.getOrAwaitValue())
     }
-    
+
+    private fun getFinances() =
+        Finance(
+            by = "",
+            executionTime = 1.0,
+            fromCache = false,
+            results = getExpectedResults(),
+            validKey = true
+        )
+
     private fun getExpectedResults() =
         Results(
             availableSources = listOf("BRL"),
@@ -123,15 +108,15 @@ class HomeViewModelTest {
             stocks = Stocks(
                 cAC = CAC(location = "Paris, French", name = "CAC 40", variation = 2.24),
                 iBOVESPA = IBOVESPA(location = "Sao Paulo, Brazil", name = "BM& F BOVESPA",
-                points = 98726.812,
-                variation = -0.33
-            ),
-            nASDAQ = NASDAQ(
-                location = "New York City, United States",
-                name = "NASDAQ Stock Market",
-                points = 11777.36,
-                variation = 0.54
-            ),
-            nIKKEI = NIKKEI(location =" Tokyo, Japan", name = "Nikkei 225", variation = -0.41)
-        ), taxes = listOf(Taxe(cdi=2.0, cdiDaily=1.9, dailyFactor=1.00007469, date="2020-10-15", selic=2.0, selicDaily=1.9)))
+                    points = 98726.812,
+                    variation = -0.33
+                ),
+                nASDAQ = NASDAQ(
+                    location = "New York City, United States",
+                    name = "NASDAQ Stock Market",
+                    points = 11777.36,
+                    variation = 0.54
+                ),
+                nIKKEI = NIKKEI(location =" Tokyo, Japan", name = "Nikkei 225", variation = -0.41)
+            ), taxes = listOf(Taxe(cdi=2.0, cdiDaily=1.9, dailyFactor=1.00007469, date="2020-10-15", selic=2.0, selicDaily=1.9)))
 }
